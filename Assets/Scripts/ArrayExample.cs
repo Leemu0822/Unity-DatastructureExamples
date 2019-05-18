d309aef6ad81bfd8517a0f32e986ec5e498b03b1
LMH\Administrator <786102800@qq.com>
2019-05-18 18:32:21 +0800

    v1.0

---
+++ Assets/Scripts/ArrayExample.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayExample : MonoBehaviour
+{
+	[SerializeField] private GameObject ball;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		float[] ballX = new float[5];
+		for (int i = 0; i < ballX.Length; i++)
+		{
+			Instantiate(ball, new Vector3(i, 0, 0), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
